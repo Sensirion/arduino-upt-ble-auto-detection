ae28d844fc8a6aa59fb8c9b5492f4ea923dfc5ef
Maximilian Paulsen <max.paulsen@sensirion.com>
2024-02-02 13:30:15 +0100

    Switch to utilizing reworked UPT Core functions and definitions

--- src/ArduinoBleSensiScan.cpp
+++ src/ArduinoBleSensiScan.cpp
@@ -16,43 +16,57 @@ void SensiScan::begin() {
 }
 
 void SensiScan::getScanResults(
-    std::map<uint16_t, std::vector<DataPoint>>& scanResults) {
-    // Deep copy cachedScanResult into scanResults
-    for (const auto& cachedScanResult : _dataPointCache) {
-        std::vector<DataPoint> dpVect;
-        std::transform(cachedScanResult.second.begin(),
-                       cachedScanResult.second.end(),
-                       std::back_inserter(dpVect), [](const DataPoint& dp) {
-                           // DataPoint lacks a copy ctor or assignment operator
-                           return DataPoint(dp.signalType, dp.value,
-                                            dp.timeStamp, dp.sourceDevice);
-                       });
-        scanResults[cachedScanResult.first] = dpVect;
+    std::map<uint16_t, std::vector<Measurement>>& scanResults) {
+    for (const auto& cachedSample : _sampleCache) {
+        scanResults[cachedSample.first] = _sampleCache[cachedSample.first];
     }
-    _dataPointCache.clear();
+    _sampleCache.clear();
 }
 
 void SensiScan::keepAlive() {
     _bleClient->keepAlive();
 }
 
-void SensiScan::onAdvertisementReceived(std::string address, std::string name,
-                                        std::string data) {
+void SensiScan::onAdvertisementReceived(const std::string address,
+                                        std::string name, std::string data) {
     uint16_t companyId = (uint16_t)data[0] << 8 | (uint8_t)data[1];
     if (companyId != COMPANY_ID_FILTER) {
         return;
     }
 
-    uint8_t sampleType = (uint8_t)data[3];
-    uint16_t deviceId = getDeviceId(data);
+    // Get MAC address as uint64_t
+    uint64_t deviceID = squashMACAddress(address);
 
-    std::vector<DataPoint> samples;
-    uint8_t error = SampleDecoder::decode(sampleType, name, data, samples);
+    // Build MetaData
+    MetaData metaData;
+    metaData.deviceID = deviceID;
+    metaData.deviceType.bleGadgetType =
+        bleGadgetTypeFromCompleteLocalName(name.c_str());
+    metaData.platform = DevicePlatform::BLE;
+
+    // extract all samples
+    std::vector<Measurement> samples;
+    uint8_t error = decodeData(metaData, data, samples);
     if (error) {
         return;
     }
 
-    _dataPointCache[deviceId] = samples;
+    // Last two digits of MAC addr. suffice to uniquely ID a BLE device
+    _sampleCache[getDeviceId(data)] = samples;
+}
+
+/**
+ * @brief squash std::string address to a uint64_t
+ * @note MAC address is comprised of 6 bytes
+ */
+uint64_t SensiScan::squashMACAddress(const std::string& macAddressString) {
+
+    uint64_t deviceID = macAddressString[0];
+    for (size_t i = 1; i < 6; i++) {
+        deviceID = (deviceID << 8) | macAddressString[i];
+    }
+
+    return deviceID;
 }
 
 /**
@@ -61,4 +75,42 @@ void SensiScan::onAdvertisementReceived(std::string address, std::string name,
 uint16_t SensiScan::getDeviceId(const std::string& data) {
     uint16_t deviceId = (uint16_t)data[4] << 8 | (uint16_t)data[5];
     return deviceId;
+}
+
+extern std::map<DataType, SampleConfig> sampleConfigSelector;
+
+/**
+ * @brief decode chunk of Advertisement containing encoded samples
+ *
+ * @returns 0 on success
+ *          1 if the Frame is too short or if the SampleType is unknown
+ */
+uint8_t SensiScan::decodeData(const MetaData& metaData, const std::string& data,
+                              std::vector<Measurement>& samples) {
+    uint8_t sampleType = static_cast<uint8_t>(data[3]);
+
+    DataType dataType = getDataTypeFromSampleType(sampleType);
+    SampleConfig sampleConfig = sampleConfigSelector[dataType];
+
+    if (data.length() < 6 + sampleConfig.sampleSizeBytes) {
+        return 1; // Frame too short or no config found
+    }
+
+    unsigned long timestamp = millis();
+
+    for (const auto& sampleSlot : sampleConfig.sampleSlots) {
+        uint16_t rawValue = getRawValue(data, 6 + sampleSlot.second.offset);
+
+        DataPoint dataPoint;
+        dataPoint.t_offset = timestamp;
+        dataPoint.value = sampleSlot.second.decodingFunction(rawValue);
+
+        Measurement measurement;
+        measurement.dataPoint = dataPoint;
+        measurement.signalType = sampleSlot.first;
+        measurement.metaData = metaData;
+
+        samples.push_back(measurement);
+    }
+    return 0;
 }
--- src/ArduinoBleSensiScan.h
+++ src/ArduinoBleSensiScan.h
@@ -9,7 +9,6 @@
 
 #include "Arduino.h"
 #include "BleClient.h"
-#include "SampleDecoder.h"
 #include "Sensirion_UPT_Core.h"
 #include <map>
 #include <string>
@@ -21,15 +20,18 @@ class SensiScan: public BleClientCallback {
 
     void begin();
     void
-    getScanResults(std::map<uint16_t, std::vector<DataPoint>>& scanResults);
+    getScanResults(std::map<uint16_t, std::vector<Measurement>>& scanResults);
     void keepAlive();
 
   private:
     BleClient* _bleClient;
-    std::map<uint16_t, std::vector<DataPoint>> _dataPointCache;
+    std::map<uint16_t, std::vector<Measurement>> _sampleCache;
     void onAdvertisementReceived(std::string address, std::string name,
-                                 std::string manufacturerData) override;
+                                 std::string data) override;
+    uint64_t squashMACAddress(const std::string& macAddressString);
     uint16_t getDeviceId(const std::string& data);
+    uint8_t decodeData(const MetaData& metaData, const std::string& data,
+                       std::vector<Measurement>& samples);
 };
 
 #endif // ARDUINO_BLE_SENSISCAN_H
